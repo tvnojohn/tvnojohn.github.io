6a9ce1cb6ae2f1c9b7e05ba74623fc6de3b3a677
tvnojohn <b1012038@fun.ac.jp>
2015-11-13 18:51:38 +0900

    loadtest20

--- js/load.js
+++ js/load.js
@@ -29,5 +29,5 @@ function createHttpRequest(){
 function loadStage(fileName){
 	var data = loadFile(fileName) + "";
 	var stageData = data.split(",");
-	console.log(stageData[0] + stageData[3]);
+	console.log(stageData);
 }	
